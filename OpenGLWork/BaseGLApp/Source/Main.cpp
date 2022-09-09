2e06774fa898ca82e9b5139758c8e4a51161f4da
joyaty <joyaty@gmail.com>
2022-09-10 02:25:48 +0800

    * 项目设置适配MacOS

--- OpenGLWork/BaseGLApp/Source/Main.cpp
+++ OpenGLWork/BaseGLApp/Source/Main.cpp
@@ -14,6 +14,9 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef _GLFW_COCOA
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
 	GLFWwindow* pWindow = glfwCreateWindow(1280, 720, "JoyOpenGLWork", NULL, NULL);
 	if (pWindow == NULL)
